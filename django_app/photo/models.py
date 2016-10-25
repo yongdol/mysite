1388f917a780c3010e7986e06715bd16d1f0cf94
yongdol <kungbuk77@gmail.com>
2016-10-25 16:32:40 +0900

    photo.model 작성

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=20)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True)
+    description = models.CharField(max_length=100)
+
+
+class Photo(models.Model):
+    Album = models.ForeignKey(Album)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=100, blank=True)
+    img = models.ImageField(upload_to='photo')
