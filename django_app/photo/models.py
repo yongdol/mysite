06f38d3ac7b01f45c500669937a28ee7f3b5763f
yongdol <kungbuk77@gmail.com>
2016-10-28 01:03:23 +0900

    utils에 BaseModel추가 및 photo.models에서 상속받도록 변경

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.conf import settings
+from mysite.utils.models import BaseModel
 
 
-class Album(models.Model):
+class Album(BaseModel):
     title = models.CharField(max_length=20)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True)
 
 
-class Photo(models.Model):
+class Photo(BaseModel):
     Album = models.ForeignKey(Album)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=20)
@@ -19,14 +20,13 @@ class Photo(models.Model):
     dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoDisLike', related_name='photo_set_dislike_users')
 
 
-class PhotoLike(models.Model):
+class PhotoLike(BaseModel):
     photo = models.ForeignKey(Photo)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_date = models.DateTimeField(auto_now_add=True)
 
 
-
-class PhotoDisLike(models.Model):
+class PhotoDisLike(BaseModel):
     photo = models.ForeignKey(Photo)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_date = models.DateTimeField(auto_now_add=True)
