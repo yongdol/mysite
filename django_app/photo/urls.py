05cf137f7f9285c5c27a8d0d9c3a72c684fcdb6f
yongdol <kungbuk77@gmail.com>
2016-10-28 02:21:09 +0900

    html,css,sass복사 및 album_detail구현

--- django_app/photo/urls.py
+++ django_app/photo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.album_list, name='album_list'),
     url(r'^album/add/$', views.album_add, name='album_add'),
+    url(r'^album/(?P<pk>[0-9]+)/$', views.album_detail, name='album_detail'),
 ]
--- django_app/photo/views/album.py
+++ django_app/photo/views/album.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 __all__ = [
     'album_list',
     'album_add',
+    'album_detail',
 ]
 
 
@@ -37,5 +38,13 @@ def album_add(request):
         'form': form,
     }
     return render(request, 'photo/album_add.html', context)
+
+
+def album_detail(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    context = {
+        'album': album,
+    }
+    return render(request, 'photo/album_detail.html', context)
 
 
